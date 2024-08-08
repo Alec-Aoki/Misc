8d3a31ee84f03f15cc37d6d3e0259d07d419714e
Alec Aoki <alec.camposaoki@gmail.com>
2024-08-08 08:51:26 -0300

    Update

---
+++ list_comp.py
@@ -0,0 +1,14 @@
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+
+#for i in range(0, x+1):
+#    for j in range(0, y+1):
+#        for k in range(0, z+1):
+#            if((i+j+k) != n):
+#                if((i == x) and (k == z)):
+#                    print("[", i, ",", j, ",", k, "]")
+#                else:
+#                    print("[", i, ",", j, ",", k, "],", end='')
